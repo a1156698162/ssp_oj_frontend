2b6a8d46470dacdafea9dc3605e8facc85a70062
ssp_gitee_gulimall <1156698162@qq.com>
2023-10-14 23:19:03 +0800

    前端项目初始化

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,92 @@
+<template>
+  <a-row id="gloablHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/闪电.svg" />
+            <div class="title">P OJ</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>
+        {{ store.state.user?.loginUser?.userName ?? "未登录" }}
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup lang="ts">
+import { routes } from "../router/routes";
+import { useRoute, useRouter } from "vue-router";
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
+import checkAccess from "@/access/checkAccess";
+import ACCESS_ENUM from "@/access/accessEnum";
+
+const router = useRouter();
+const store = useStore();
+
+const visibleRoutes = computed(() => {
+  return routes.filter((item, index) => {
+    if (item.meta?.hideInMenu) {
+      return false;
+    }
+    if (
+      !checkAccess(store.state.user.loginUser, item?.meta?.access as string)
+    ) {
+      return false;
+    }
+    return true;
+  });
+});
+
+const selectedKeys = ref(["/"]);
+
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+setTimeout(() => {
+  store.dispatch("user/getLoginUser", {
+    userName: "pp",
+    userRole: ACCESS_ENUM.ADMIN,
+  });
+}, 3000);
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  height: 48px;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+</style>
